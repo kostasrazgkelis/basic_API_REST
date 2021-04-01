af0d16ae7954d42e8b66e7d206397872f01435b7
Kostas Razg <100526759@unimail.derby.ac.uk>
2021-04-01 22:57:48 +0300

    small complete example

--- api.py
+++ api.py
@@ -1,21 +1,71 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource , fields, marshal_with
 
-app = Flask(__name__)
-api = Api(app)
+app = Flask( __name__ )
+api = Api( app )
 
-todos = {}
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+    'todo4': {'task': 'anohter stuff!'},
+}
 
-class TodoSimple(Resource):
 
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort( 404, message="Todo {} doesn't exist".format( todo_id ) )
+
+
+parser = reqparse.RequestParser()
+parser.add_argument( 'task', type=int, help='The task coudlbt be converented to integer' )
+
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo( Resource ):
     def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+        abort_if_todo_doesnt_exist( todo_id )
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist( todo_id )
+        del TODOS[todo_id]
+        return '', 204
 
     def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList( Resource ):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int( max( TODOS.keys() ).lstrip( 'todo' ) ) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+
+class PrintWord( Resource ):
+    def get(self):
+        return {'message': 'Congratualtion there is something here'}
+
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource( TodoList, '/todos' )
+api.add_resource( Todo, '/todos/<todo_id>' )
+api.add_resource( PrintWord, '/print' )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run( debug=True )
